209989a57d47beb69080614f38a0e93302311a1b
agatakovvalska <agata.kovvalska@wp.pl>
2024-11-13 12:11:27 +0100

    Koniec zajęć w dniu 13.11.2024
    Na ten moment jest ok

--- R/src/Skrypt01.R
+++ R/src/Skrypt01.R
@@ -1,2 +1,19 @@
 # Załadowanie paczek
 library(tidyverse)
+
+# Dane New Zealnd (z www)
+dane_www <- readr::read_csv(file = "https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv")
+
+# Wyczytanie z dysku - pełna ścieżka (bezwzględna)
+dane_dysk_full_path <- readr::read_csv(file = "C:/GitHub/przetwarzanie_danych_projekt_semestralny/R/data/annual-enterprise-survey-2023-financial-year-provisional.csv")
+
+# Wczytanie z dysku - ścieżka względna
+dane_dysk_relative_path <- readr::read_csv(file = "R/data/annual-enterprise-survey-2023-financial-year-provisional.csv")
+
+# Zmiana kolumny tekstowej na liczbowe
+dane_dysk_relative_path$Value <- as.numeric(dane_dysk_relative_path$Value)
+
+#Parsowanie tekstu na numer
+dane <- readr::read_csv(file = "R/data/annual-enterprise-survey-2023-financial-year-provisional.csv")
+dane$Value <- readr::parse_number(dane$Value)
+dane[748, "Value"]
